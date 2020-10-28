b5e87d8f88b548bd7f3f93f5e7329ab25e55642e
Daniel Barreto <danielbarreto559@gmail.com>
2020-10-27 21:12:19 -0300

    Primeiro Comit

---
+++ Ada/Ada/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
