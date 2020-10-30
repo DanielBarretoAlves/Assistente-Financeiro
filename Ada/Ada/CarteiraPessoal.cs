9fe0c32002d1b591c0d6dd8d6c1949a210a78c24
Daniel Barreto <danielbarreto559@gmail.com>
2020-10-29 22:33:27 -0300

    Classe Carteira Pessoal

---
+++ Ada/Ada/CarteiraPessoal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ada
+{
+    class CarteiraPessoal
+    {
+        private float budget;
+        private Gasto[] gastos;
+        private Renda[] rendas;
+    }
+}
