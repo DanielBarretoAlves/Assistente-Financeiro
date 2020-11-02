5a4de1dff5d378cbfea93ef347a32feef2891415
your username <rodrigodjr1@gmail.com>
2020-11-02 17:44:57 -0300

    Salario

---
+++ Ada/Ada/Salario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ada
+{
+    class Salario
+    {
+
+        public void adicionarHoraExtra(int valHora, int numHoras) { }
+
+        public void calcComissão() { }
+    }
+}
