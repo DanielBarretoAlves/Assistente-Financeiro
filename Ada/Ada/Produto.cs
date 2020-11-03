04935e70391e3065df61811c0fbc1f943eeab014
your username <rodrigodjr1@gmail.com>
2020-11-03 19:51:39 -0300

    Reupando Produto

---
+++ Ada/Ada/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ada
+{
+    public abstract class Produto
+    {
+
+        int valor;
+        int tipo;
+
+        public virtual void calcJuros() { }
+    }
+}
