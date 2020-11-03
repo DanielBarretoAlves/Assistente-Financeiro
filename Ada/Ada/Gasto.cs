b5fff975d4bac1e599b98fab35ea668b3d6c58b8
elioliveir <71047243+elioliveir@users.noreply.github.com>
2020-11-03 19:42:59 -0300

    Classe Gasto

---
+++ Ada/Ada/Gasto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ada
+{
+    public class Gasto
+    {
+        private int categoria;
+        private int importancia;
+        private int incremental;
+        //teste
+
+    }
+}
