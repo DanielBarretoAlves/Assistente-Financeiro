426520a8a5dbd552238886193caef4ee6a47e7db
Daniel Barreto <danielbarreto559@gmail.com>
2020-11-23 18:11:16 -0300

    Update Migration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Json;
-using Newtonsoft.Json;
 
 namespace Ada
 {
@@ -9,13 +6,7 @@ namespace Ada
     {
         static void Main(string[] args)
         {
-
-            // Controller ada = new Controller();
-            Controller ada = new Controller();
-            ada.addCarteira();
-            // p.addRenda();         
-         
-            // Console.WriteLine(p.Agenda[0].Gastos[0].Nome);
+            Console.WriteLine("Hello World!");
         }
     }
 }
